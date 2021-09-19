ad8aad4a80cecf9172e1e85ff85476ab02d04f73
rowlandwilliams <rowland.williams.94@gmail.com>
2021-09-19 12:49:35 +0100

    fix voronoi

--- src/components/NavBar/Voronoi/Voronoi.jsx
+++ src/components/NavBar/Voronoi/Voronoi.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { generateRandomPoints, plot } from "./utils/plot-utils";
 import "./styles.css";
-import { Link } from "react-router-dom";
 
 const voronoiWidth = 64;
 const padding = 1;
@@ -9,8 +8,9 @@ const padding = 1;
 const Voronoi = () => {
   // randomly pick color scheme
 
-  
-  var pointSeed = 5; 
+  // number of initial polygons
+  // var pointSeed = 10;
+  var pointSeed = 5; // Math.floor(Math.random() * 8) + 5;
 
   var pickColor = Math.random() < 0.5;
 
@@ -27,8 +27,8 @@ const Voronoi = () => {
   });
 
   return (
-    <div className="w-10 h-10 md:w-16 md:h-16 rounded-md overflow-hidden transition-all duration-150 border-1 border-gray-800 hover:border-white ">
-      <svg width="100%" height="100%" id="voronoi">
+    <div className="w-10 h-10 md:w-16 md:h-16 rounded-lg overflow-hidden transition-all duration-150 border-2 border-gray-800 hover:border-white ">
+      <svg width="100%" height="100%" class="chart">
         <defs>
           <clipPath id="rect-clip">
             <rect
