782beca004ea8a6ef82227aa91a8cde4acd6df81
rowlandwilliams <rowland.williams.94@gmail.com>
2021-09-20 15:06:39 +0100

    update footer

--- src/components/About/Footer/Footer.tsx
+++ src/components/About/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="border-gray-400 border-t-1 py-8 px-2 md:px-0 w-3/4">
+    <div className="border-gray-400 border-t-1 py-8 px-2 md:px-0 ">
       <div className="text-base font-inter-medium pb-4">Rowland Williams</div>
       <div className="text-sm">
         <div>Cartographic / Data Visualization Specialist</div>
