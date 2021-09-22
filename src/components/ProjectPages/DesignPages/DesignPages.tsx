bef2ad6e71a3c173c6ce49bf60840c95c1119d8b
rowlandwilliams <rowland.williams.94@gmail.com>
2021-09-22 15:36:13 +0100

    fix design nav error

--- src/components/ProjectPages/DesignPages/DesignPages.tsx
+++ src/components/ProjectPages/DesignPages/DesignPages.tsx
@@ -1,7 +1,6 @@
 import { Route } from "react-router";
 import { ImgData } from "../../../types/types";
 import { designProjects } from "../../Grid/grid-data/designProjects";
-import { graphProjects } from "../../Grid/grid-data/graphProjects";
 import { importAllImages } from "../../utils/utils";
 import { PageTemplate } from "../PageTemplate/PageTemplate";
 
@@ -24,9 +23,9 @@ export const DesignPages = () => {
               const re = new RegExp(project.imgSlug, "g");
               return image.slug.match(re);
             })}
-            previousProjectLink={i !== 0 ? graphProjects[i - 1].imgSlug : null}
+            previousProjectLink={i !== 0 ? designProjects[i - 1].imgSlug : null}
             nextProjectLink={
-              i !== nProjects - 1 ? graphProjects[i + 1].imgSlug : null
+              i !== nProjects - 1 ? designProjects[i + 1].imgSlug : null
             }
             gridName="design"
           />
